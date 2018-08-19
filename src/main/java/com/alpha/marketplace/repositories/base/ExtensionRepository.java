c4b330e70347c643ce97edafd4bb7198e4d8a79e
Alexander <troqta@gmail.com>
2018-08-19 19:00:43 +0300

    Implemented most of ExtensionRepository methods

--- src/main/java/com/alpha/marketplace/repositories/base/ExtensionRepository.java
+++ src/main/java/com/alpha/marketplace/repositories/base/ExtensionRepository.java
@@ -1,5 +1,25 @@
 package com.alpha.marketplace.repositories.base;
 
+import com.alpha.marketplace.models.Extension;
+
+import java.util.Date;
+import java.util.List;
+
 public interface ExtensionRepository {
+
+    List<Extension> getAll();
+    List<Extension> getSelectedByAdmin();
+    List<Extension> getNewest();
+    List<Extension> getMostPopular();
+    Extension getById(int id);
+    Extension getByName(String name);
+    List<Extension> getByDownloads(int downloads);
+    List<Extension> getByUploadDate(Date date);
+    List<Extension> getByCommitDate(Date date);
+    boolean save(Extension extension);
+    boolean update(Extension extension);
+    boolean delete(int id);
+
+
 
 }
