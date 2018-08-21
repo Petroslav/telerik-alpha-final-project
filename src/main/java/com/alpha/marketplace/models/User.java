a70490e3a90c320cfef3b99fad340e5b15817ea0
Pepi <petroslav.angelov@gmail.com>
2018-08-21 20:41:11 +0300

    Possibly fixed login issue, csfr is disabled, starting work on private part of app

--- src/main/java/com/alpha/marketplace/models/User.java
+++ src/main/java/com/alpha/marketplace/models/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
