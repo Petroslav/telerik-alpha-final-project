d19296f355f53b2f8bd72fb4c3786e0b4a04f916
Alexander <troqta@gmail.com>
2018-08-23 01:28:56 +0300

    REEEEEEEEE COMMIT(header... and security... and not header ... stuff)

--- src/main/java/com/alpha/marketplace/config/SecurityConfiguration.java
+++ src/main/java/com/alpha/marketplace/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/register", "/user/public/**", "/upload", "/upload/kek").permitAll()
+                .antMatchers("/", "/register", "/user/public/**", "/upload", "/upload/kek", "/css/**", "/scripts/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
---
+++ src/main/resources/static/scripts/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $('.home-icon').on('click', function(){
+        $('html').animate({ scrollTop: 0 }, 'slow'); return true;
+    });
+
+    var totalH = $('#stickyNav').offset().top;
+    $(window).bind('scroll', function () {
+        var vPos = $(window).scrollTop();
+
+        if (totalH < vPos) {
+            $('#stickyNav').css({
+                'position': 'fixed',
+                'top': 0,
+                'bottom' : 'auto'
+            })
+        } else {
+            $('#stickyNav').css({
+                'position': 'absolute',
+                'top': '',
+                'bottom': 0
+            })
+        }
+    });
+});
+
