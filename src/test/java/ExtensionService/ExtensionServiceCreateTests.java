c3e2536c2b0d13e632ac04a80b7b2a68979ca6d4
Pepi <petroslav.angelov@gmail.com>
2018-09-02 17:08:10 +0300

    Removed unnecessary test class.

--- src/test/java/ExtensionService/ExtensionServiceCreateTests.java
+++
@@ -1,8 +0,0 @@
-package ExtensionService;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ExtensionServiceCreateTests {
-}
